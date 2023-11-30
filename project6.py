8f07c4c1de3a83aa79ee41df2a9c8b12197ec76a
Nethajiparthinan <Nethajiparthiban@gmail.com>
2023-11-30 07:21:15 +0530

    project6 with 41% accuracy

---
+++ project6.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+data=pd.read_csv(r"D:\Git\Git-Projects\IQ_size.csv")
+X=data.iloc[:,:-1]
+Y=data.iloc[:,-1]
+#training
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.25,random_state=1)
+#fitting the dataset.
+from sklearn.linear_model import LinearRegression
+stud=LinearRegression()
+stud.fit(x_train,y_train)
+#predicting.
+y_pred=stud.predict(x_test)
+#checking the accuracy.
+from sklearn.metrics import mean_squared_error,r2_score
+rscore=r2_score(y_test,y_pred)
+rs=rscore*100
+print(rs.round(),'%')
